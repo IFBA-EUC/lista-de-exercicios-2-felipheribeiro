b9ebb06a666c980b0f7ba8613a5f50bb6b27fcd4
felipheribeiro <89546698+felipheribeiro@users.noreply.github.com>
2021-08-27 20:50:56 -0300

    Create questao5.java

---
+++ questao5.java
@@ -0,0 +1,14 @@
+public class main {
+
+            public static void main(String[] args){
+
+                 for (int nmr1 = 1; num1 < 9; nmr1++){ 
+                      System.out.println("\nTabela de "+nmr1); 
+
+                      for (int nmr2 = 1; nmr2 <= 10; nmr2++){ 
+                      System.out.print(nmr1 + " x " + nmr2 + " = " + nmr1 * nmr2 + "  ");
+                      }
+
+                 }
+
+            }
